16537409f83227016934ad01732b44b89d48239b
@yrauvtovich <eravtovich@mail.ru>
2020-10-02 12:44:14 +0300

    01/10

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-    $(".circle")
-        .mouseenter(function() {
-
-        })
+    $(".circle-one").mouseenter(function() {
+        $(".circle-one").addClass( "circle-two" );
+        $( ".circle-two" ).animate({
+            opacity: "1"
+        }, 1500, "linear");
+        $(".circle-one").addClass( "circle-three" );
+        $( ".circle-three" ).animate({
+            opacity: "1"
+        }, 30000, "linear");
+        $(".circle-one").addClass( "circle-four" );
+        $( ".circle-four" ).animate({
+            opacity: "1"
+        }, 60000, "linear");
+        $(".circle-one").addClass( "circle-five" );
+        $( ".circle-five" ).animate({
+            opacity: "1"
+        }, 120000, "linear");
+    })
 });
 

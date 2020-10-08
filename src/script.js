dd169da8d2c32cac392d4af447353c2003ec362a
@yrauvtovich <eravtovich@mail.ru>
2020-10-08 14:03:18 +0300

    08/10

--- src/script.js
+++ src/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
  $( ".circle-one" ).mouseenter(function() {
-		$( ".circle-one" ).animate({
+ 	$( ".circle-one" ).animate({
 			opacity: "1",
 			width: "18vmin",
 			height: "18vmin"
 		}, 1500);
-		$(".circle-two" ).animate({
+ 	$(".circle-two" ).animate({
 			opacity: "1",
 			width: "38vmin",
 			height: "38vmin"
 		}, 1800);
-		$( ".circle-three" ).animate({
+ 	$( ".circle-three" ).animate({
 			opacity: "1",
 			width: "58vmin",
 			height: "58vmin"
 		}, 2100);
-		$( ".circle-four" ).animate({
+ 	$( ".circle-four" ).animate({
 			opacity: "1",
 			width: "78vmin",
 			height: "78vmin"
 		}, 2400);
-		$( ".circle-five" ).animate({
+ 	$( ".circle-five" ).animate({
 			opacity: "1",
 			width: "98vmin",
 			height: "98vmin"
 		}, 2700);
-  });
+ });
  $( ".circle-one" ).mouseleave(function() {
   	$( ".circle-one" ).animate({
 			opacity: "1",
